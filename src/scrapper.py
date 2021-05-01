7f47c103469340cc35b41fe80743845f2334e2bb
Carlos Sousa <csousa90@gmail.com>
2021-05-01 23:52:37 +0200

    Main logic done. Will write movies to movies.csv and shows to series.csv

--- src/scrapper.py
+++ src/scrapper.py
@@ -2,6 +2,7 @@ import os
 import time
 import json
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 def cls():
@@ -9,11 +10,46 @@ def cls():
 
 def main():
     cls()
-    print("Hey2")
+    baseURL     = "https://www.imdb.com/title/tt"
+    startURL    = 0
+    endURL      = 9999999
 
 
 
 
+    table = []
+    for i in range(startURL, endURL):
+        titleFixed = str(i).zfill(7)
+        url = baseURL + titleFixed + '/'
+        row = []
+        try:
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, 'html.parser')
+            data = json.loads(soup.find('script', type='application/ld+json').string)
+
+            if(data['@type'] == 'Movie'):
+                f = open("/opt/storage/movies.csv", "a")
+            if(data['@type'] == 'TVSeries'):
+                f = open("/opt/storage/series.csv", "a")
+
+            write = csv.writer(f)
+            row.append(data['name'])
+            row.append(url)
+            row.append(data['genre'])
+            row.append(data['aggregateRating']['ratingValue'])
+            try:
+                row.append(data['datePublished'])
+            except Exception as e:
+                pass
+            write.writerow(row)
+            f.close()
+            print(row)
+        except Exception as e:
+            print(e)
+            '''
+            # TODO:
+            If Error, output to log file
+            '''
 
 
 if __name__ == "__main__":
