49999129890e3143596dc8bdbf730bcffd84dd16
Carlos Sousa <csousa90@gmail.com>
2021-05-03 02:49:25 +0200

    Trying I/O improvements

--- src/scrapper.py
+++ src/scrapper.py
@@ -10,27 +10,29 @@ from bs4 import BeautifulSoup
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def imdbscrapper(i):
+def imdbscrapper(currentProcess):
     cpuCount        = int(os.cpu_count()) - 1
     baseURL         = "https://www.imdb.com/title/tt"       # Base URL for each title
     startURL        = 9999999                               # Start Number
     endURL          = 0                                     # Ending Number
     debugLevel      = 40                                    # 20 will display Info messages, 40 errors
-    logFile         = "/opt/storage/info.log"               # Log output
-    counterFileBase = "/opt/storage/counter"            # Base for the counter file
+    #logFile         = "/opt/storage/info.log"               # Log output
+    logFileBase     = "/opt/storage/info"                   # Base for the logger file
+    logFileExt      = ".log"                                # Extension for the logger file
+    counterFileBase = "/opt/storage/counter"                # Base for the counter file
     counterFileExt  = ".txt"
     #counterFile     = "/opt/storage/counter.txt"            # Which ID was last scanned
     reCheckFile     = "/opt/storage/recheck.txt"            # Which IDs to recheck
 
 
-    startURL = int((startURL/cpuCount)*i)
-    endURL = int(i*(startURL/cpuCount)+1)
+    startURL = int((startURL/cpuCount)*currentProcess)
+    endURL = int(currentProcess*(startURL/cpuCount)+1)
 
-
+    logFile = logFileBase + str(currentProcess) + logFileExt
     try:
         # Tries to read the value to continue from counterN.txt. On error defaults to StartURL
-        counterFile = counterFileBase + str(i) + counterFileExt
-        counter = open(counterFile, "r")
+        counterFile = counterFileBase + str(currentProcess) + counterFileExt
+        counter = open(counterFile, "r", os.O_NONBLOCK)
         startURL = int(counter.read())
         counter.close()
     except Exception as e:
@@ -48,9 +50,9 @@ def imdbscrapper(i):
 
             # Sorting into Movies and Shows, based on a file
             if(data['@type'] == 'Movie'):
-                f = open("/opt/storage/movies.csv", "a")
+                f = open("/opt/storage/movies.csv", "a", os.O_NONBLOCK)
             if(data['@type'] == 'TVSeries'):
-                f = open("/opt/storage/series.csv", "a")
+                f = open("/opt/storage/series.csv", "a", os.O_NONBLOCK)
 
             # Gets the desired values from the JSON response
             row.append(data['name'])
@@ -76,11 +78,21 @@ def imdbscrapper(i):
             except Exception as e:
                 row.append("Unknown")
             # Opens, writes and closes the file with the information of the movie / series
-            write = csv.writer(f)
-            write.writerow(row)
-            if(debugLevel <= 20):
-                logging.info(row)
-            f.close()
+            sucessTest = False
+            while sucessTest != True:
+                try:
+                    for entry in row:
+                        print(entry)
+                    '''
+                    write = csv.writer(f)
+                    write.writerow(row)
+                    if(debugLevel <= 20):
+                        logging.info(row)
+                    f.close()
+                    sucessTest = True
+                    '''
+                except Exception as e:
+                    print("Retrying updating")
 
             print(row)      # Prints to the screen, in case the user is watching the Docker / software in foreground
         except Exception as e:
@@ -89,7 +101,7 @@ def imdbscrapper(i):
             logging.error(errorMessage)
             # If the error is page not available, add to file (Perforance improvement on rechecks)
             if("NoneType" in str(e)):
-                recheck = open(reCheckFile, "a")
+                recheck = open(reCheckFile, "a", os.O_NONBLOCK)
                 recheckString = titleFixed + "\n"
                 recheck.write(recheckString)
                 recheck.close()
